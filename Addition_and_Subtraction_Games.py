fc97b2cfc5131acbafe88c95ed7d998b91eba2dd
KaiYuanee <115628863+KaiYuanee@users.noreply.github.com>
2022-12-31 23:50:05 +0800

    Update Addition_and_Subtraction_Games.py

--- Addition_and_Subtraction_Games.py
+++ Addition_and_Subtraction_Games.py
@@ -2,6 +2,49 @@
 # Made by KaiYuanee
 # Copyright(C) 2022-2023 KaiYuanee Foundation, Inc.
 
+basic.show_leds("""
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    """)
+
+def neverGonnaGiveYouUp():
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+
+    basic.pause(200)
+
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(175, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+
+
 signList = ['+', '-']
 def main():
     sign = randint(0, 1)
@@ -20,12 +63,51 @@ def main():
     # Addend and subtrahend are B
     # The result is C
     
+    # Show the question
     basic.show_number(A)
-    basic.pause(1000)
+    basic.pause(300)
     basic.show_string(signList[sign])
-    basic.pause(1000)
+    basic.pause(300)
     basic.show_number(B)
-    basic.pause(1000)
+    basic.pause(300)
     basic.show_string('=')
 
+    # User inputs the answer
+    Ans = 0
+    while not(input.button_is_pressed(Button.AB)):
+        if input.button_is_pressed(Button.A):
+            Ans -= 1
+        elif input.button_is_pressed(Button.B):
+            Ans += 1
+
+        # Set the range from 0 to 9
+        if Ans == -1:
+            Ans = 9
+        elif Ans == 10:
+            Ans = 0
+
+        basic.show_number(Ans)
+    
+    # Judge if the answer is correct
+    if C == Ans:
+        basic.show_leds("""
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        """)
+        music.play_tone(392, music.beat(BeatFraction.WHOLE))
+    elif C != Ans:
+        basic.show_leds("""
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        """)
+        music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.pause(2000)
+
+neverGonnaGiveYouUp()
 basic.forever(main)
